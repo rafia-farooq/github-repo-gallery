cb59892a0fa6e1578ff6e3d7acaee3659df62b62
r-farooq <rafiafarooq@hotmail.com>
2022-11-01 18:35:59 +0300

    error corrected in javascript function

--- js/script.js
+++ js/script.js
@@ -3,14 +3,16 @@ const repoList = document.querySelector(".repo-list");
 const repoData = document.querySelector(".repo-data");
 
 
-const username = "rafia-farooq"
+const username = "rafia-farooq";
+
+
 const personalData = async function () {
     const response = await fetch (`https://api.github.com/users/${username}`);
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
     
     displayData(data);
-}
+};
 
 personalData();
 
@@ -48,5 +50,23 @@ const displayData = function ( data) {
     <p> <strong>Bio: </strong> ${bio}</p>
     <p> <strong>Location: </strong> ${location}</p>
     <p> <strong>Number of Public Repos: </strong> ${numOfRepos}</p>`;
-    info.append(p);
-}
+};
+
+// or a shorter way:
+// 
+// const displayUserInfo = function (data) {
+//     const div = document.createElement("div");
+//     div.classList.add("user-info");
+//     div.innerHTML = `
+//       <figure>
+//         <img alt="user avatar" src=${data.avatar_url} />
+//       </figure>
+//       <div>
+//         <p><strong>Name:</strong> ${data.name}</p>
+//         <p><strong>Bio:</strong> ${data.bio}</p>
+//         <p><strong>Location:</strong> ${data.location}</p>
+//         <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+//       </div>
+//     `;
+//     overview.append(div);
+//   };
